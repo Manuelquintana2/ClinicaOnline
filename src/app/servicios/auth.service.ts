c26551156e8dc3ad993a5bd61a9baed6681b0529
Manuel Quintana <manu.quintanamino@gmail.com>
2025-06-04 12:18:51 -0300

    terminamos home y navbar

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../../enviroment/enviroment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private supabase : SupabaseClient;
+
+  constructor() {
+    this.supabase = createClient(
+      environment.supabaseUrl,
+      environment.supabaseAnonKey
+    );
+   }
+
+   async signIn(email: string, password: string) {
+    const { data, error } = await this.supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+  
+    if (error) throw error;
+    return data;
+  }
+
+  async signOut() {
+    const { error } = await this.supabase.auth.signOut();
+    if (error) throw error;
+  }
+  
+  onAuthStateChange(callback: (event: string, session: any) => void) {
+    this.supabase.auth.onAuthStateChange((event, session) => {
+      callback(event, session);
+    });
+  }
+
+  getCurrentUser() {
+    return this.supabase.auth.getUser();
+  }
+  
+  getClient(): SupabaseClient {
+    return this.supabase;
+  }
 }
